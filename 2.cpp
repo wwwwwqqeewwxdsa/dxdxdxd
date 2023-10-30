c1ffc843fdbbe211bb7594fce54d650793d6c535
Dmitro Bondarenko (Uczeń) <dbondarenko792@technikumkreatywne.pl>
2023-10-30 12:00:06 +0100

    Kommentarz

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	short a, b;
+	cout<<"Podaj a:";
+	cin>>a ;
+	cout<< "Podaj b:";
+	cin>>b;
+	if (a > b){
+		cout<< "a > b";
+	}
+	else {
+		cout<< "a < b";
+	}
+	
+
+}
