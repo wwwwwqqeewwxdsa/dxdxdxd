07dfc3e1f0341eabd6ba53c0428adff1c31e5201
Dmitro Bondarenko (Uczeń) <dbondarenko792@technikumkreatywne.pl>
2023-11-13 11:59:05 +0100

    Code

---
+++ 4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+   
+   int main (){
+   	int szerokosc, wysokosc;
+   	cout << "Podaj szerokosc prostokata" ;
+   	cin >> szerokosc;
+   	cout << "Podaj wysokosc prostokata";
+   	cin >> wysokosc;
+   	 for (int i = 0; i < wysokosc; ++i){
+   	  for (int j = 0; j < szerokosc; ++j){
+   	  	if (i = 0 || i == wysokosc - 1 || j == 0 || j == szerokosc - 1){
+   	  	cout << " * ";
+   		 } else {
+			
+   		cout << "*" ;
+   	}
+   }
+}
+	   
+   			cout << endl;
+   			return 0;
+   }
